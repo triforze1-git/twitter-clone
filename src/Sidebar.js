f1f143cf4489466e3ecca345e9b3d30527a9f70b
triforze1.git <triforze1.git@gmail.com>
2021-10-17 10:51:28 -0400

    second commit

---
+++ src/Sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizonIcon from '@material-ui/icons/MoreHoriz';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            {/* Twitter Icon */}
+            <TwitterIcon />
+            <SidebarOption Icon={HomeIcon} text="Home"/>
+            <SidebarOption Icon={SearchIcon} text="Explore"/>
+            <SidebarOption Icon={NotificationsNoneIcon} text="Notifications"/>
+            <SidebarOption Icon={MailOutlineIcon} text="Messages"/>
+            <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks"/>
+            <SidebarOption Icon={ListAltIcon} text="Lists"/>
+            <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
+            <SidebarOption Icon={MoreHorizonIcon} text="More"/>
+        </div>
+    )
+}
+
+export default Sidebar;

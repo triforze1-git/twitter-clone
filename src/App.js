e606d029b5881299d432f06f416d57329ced7ac8
triforze1-git <triforze1.git@gmail.com>
2021-10-17 12:23:10 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+import Feed from './Feed';
+import Widgets from './Widgets';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Sidebar />
+
+      <Feed />
+
+      <Widgets />
+
+    </div>
+  );
+}
+
+export default App;
